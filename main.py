cdf7198b6e3a3bfe6c67d0716306357d178b686f
Star_Wasp7272 <star.wasp7272@gmail.com>
2025-11-22 17:53:53 +0100

    add comments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
+# Setting up web app title
 st.title('Weather Forecast for the next days')
+# Adding place input box
 place = st.text_input("Place: ")
+# Adding slider for day selection
 days = st.slider(
     "Forecast days",
     min_value=1,
     max_value=5,
     help="Number of forecasted days")
+# Adding selection bax for type of data selection
 options = st.selectbox("Select data you want to view", ("Temperature", "Sky"))
+# Subheader to make it easier for user to understand what is being displayed
 st.subheader(f"{options} for the next {days} days in {place}")
 
