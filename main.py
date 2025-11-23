0cd6d4a53024f914c6fee9825cd3a4b6320388ba
Star_Wasp7272 <star.wasp7272@gmail.com>
2025-11-23 11:43:53 +0100

    linking backend and frontend. accounting for KeyError. final polish. adding pictures for weather data representation.

--- main.py
+++ main.py
@@ -15,11 +15,28 @@ days = st.slider(
 # Adding selection bax for type of data selection
 options = st.selectbox("Select data you want to view", ("Temperature", "Sky"))
 # Subheader to make it easier for user to understand what is being displayed
-st.subheader(f"{options} for the next {days} days in {place}")
+st.subheader(f"{options} for the next {days} days in {place.title()}")
+if place:
+    try:
+        # Get Temperature/Sky data
+        filtered_data = get_data(place, days)
 
-get_data(place, days, options)
+        if options == "Temperature":
+            # Getting temperature data
+            temperatures = [dict["main"]["temp"]-273.15 for dict in filtered_data]
+            # Getting days
+            days = [dict["dt_txt"] for dict in filtered_data]
+            # Creating a temperature plot
+            figure = px.line(x=days, y=temperatures, labels={"x": "dates", "y": "Temperature (C)"})
+            # Adding graph
+            st.plotly_chart(figure)
 
-# Creating plotly figure
-figure = px.line(x=d, y=t, labels={"x": "dates", "y": "Temperature (C)"})
-# Adding graph
-st.plotly_chart(figure)
+        if options == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            # Making dictionary of image filepaths
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        message = f"I'm sorry but {place.title()} does not exist. Please try again 😉"
+        st.write(message)
